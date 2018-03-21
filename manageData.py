6eb4d30f05d6aa83d529054f15b1e6a3e1c5f330
orti121 <orti121@gmail.com>
2018-03-21 19:21:54 -0300

    Add files via upload
    En la noche lo actualizo con el código

---
+++ manageData.py
@@ -0,0 +1,18 @@
+# objetos de data de 16*256. Lista de 16 diccionarios de tamaño 256 donde se almacenarán las mediciones.
+
+class DataBuff:
+	
+	def __init__(self,channels,bufsize):
+
+		blankdict={}
+		blanklist=[]
+		#llenar el diccionario donde llegará la data
+		#data.datos
+		#data.index
+
+	def add_data(datavector):
+		#Agrega los datos en secuencia, resetea y agrega en la primera posición si el buffer está lleno
+
+
+	def flush():
+		#Resetea el arreglo
